8d4ea595d255aa67965d2689cdf90a377e667496
Kevin Ruscoe <hello@kevinruscoe.me>
2020-11-09 22:43:15 +0000

    things

--- resources/js/components/fieldtypes/PositionFieldtype.vue
+++ resources/js/components/fieldtypes/PositionFieldtype.vue
@@ -1,51 +1,63 @@
 <template>
 
-    <div class="position-fieldtype-selector">
-        <a
+    <div class="position-fieldtype">
+        <button
             v-on:click.prevent="toggleModalVisibility()"
-            v-bind:class="modalToggleButtonClass"
-            href="#open-modal">
-            Set Position
-        </a>
+            v-bind:class="toggleModalClass"
+            type="button">
+            <span class="position-fieldtype__toggle-modal__icon">
+                <component v-bind:is=value />
+            </span>
+            {{ value ? value : 'Set Position' }}
+        </button>
 
-        <div
-            v-show="modalIsOpen"
-            class="position-fieldtype-selector__modal">
-            <div class="position-fieldtype-selector__modal__content">
+        <div v-bind:class="modalClass">
 
-                <div class="position-fieldtype-selector__compass">
-                    <a
-                        v-for="(position, index) in positions"
-                        v-on:click.prevent="updatePosition(position)"
-                        v-bind:key="index"
-                        v-bind:class="compassButtonClass(position)"
-                        href="#select-position">{{ position }}</a>
-                </div>
-
+            <div class="position-fieldtype__compass">
+                <button
+                    v-for="(position, index) in positions"
+                    v-on:click.prevent="updatePosition(position)"
+                    v-bind:key=index
+                    v-bind:class="{ active: (value == position) }"
+                    class="position-fieldtype__compass__button btn"
+                    type="button">
+                    <span class="hidden">{{ position }}</span>
+                    <component v-bind:is=position />
+                </button>
             </div>
+
         </div>
     </div>
 
 </template>
 <script>
+
+    import IconTopLeft from "./../icons/IconTopLeft";
+    import IconTop from "./../icons/IconTop";
+    import IconTopRight from "./../icons/IconTopRight";
+    import IconLeft from "./../icons/IconLeft";
+    import IconCenter from "./../icons/IconCenter";
+    import IconRight from "./../icons/IconRight";
+    import IconBottomLeft from "./../icons/IconBottomLeft";
+    import IconBottom from "./../icons/IconBottom";
+    import IconBottomRight from "./../icons/IconBottomRight";
+
     export default {
+        components: {
+            'top-left': IconTopLeft,
+            'top': IconTop,
+            'top-right': IconTopRight,
+            'left': IconLeft,
+            'center': IconCenter,
+            'right': IconRight,
+            'bottom-left': IconBottomLeft,
+            'bottom': IconBottom,
+            'bottom-right': IconBottomRight,
+        },
         mixins: [Fieldtype],
         methods: {
             toggleModalVisibility(){
                 this.modalIsOpen = ! this.modalIsOpen;
-            },
-            compassButtonClass(position) {
-                let cssClasses = [
-                    'btn',
-                    'position-fieldtype-selector__compass__button',
-                    `position-fieldtype-selector__compass__button--${position}`
-                ];
-
-                if (position == this.value) {
-                    cssClasses.push('active');
-                }
-
-                return cssClasses;
             },
             updatePosition(position) {
                 this.update(position);
@@ -54,12 +66,28 @@
             }
         },
         computed: {
-            modalToggleButtonClass() {
-                return [
-                    'btn',
-                    'position-fieldtype-selector__toggle-button',
-                    `position-fieldtype-selector__button--${this.value}`
+            toggleModalClass() {
+                let cssClasses = [
+                    'position-fieldtype__toggle-modal',
+                    'btn'
                 ];
+
+                if (this.modalIsOpen) {
+                    cssClasses.push('position-fieldtype__toggle-modal--inactive');
+                }
+
+                return cssClasses;
+            },
+            modalClass() {
+                let cssClasses = [
+                    'position-fieldtype__modal',
+                ];
+
+                if (this.modalIsOpen) {
+                    cssClasses.push('position-fieldtype__modal--active');
+                }
+
+                return cssClasses;
             }
         },
         data() {
